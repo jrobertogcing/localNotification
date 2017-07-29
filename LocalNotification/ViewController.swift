0d348c3b572c40e4014c5d928e2c109f8276ea75
Robert <Robert@macbook.local>
2017-07-29 09:28:16 -0500

    Initial Commit

---
+++ LocalNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotification
+//
+//  Created by Robert on 29/07/17.
+//  Copyright © 2017 Robert González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
