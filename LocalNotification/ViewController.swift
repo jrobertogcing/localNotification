46862c4d0177f44a0afb05ee9d38636aeb7d1082
Robert <Robert@macbook.local>
2017-08-10 09:47:58 -0500

    commit Init

--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var tapGesture: UITapGestureRecognizer!
+    
+    
+    @IBOutlet weak var textPrueba: UITextField!
+    
+    
+    @IBOutlet weak var textDate: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in})
+        
+        
+       
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func notificationButton(_ sender: UIButton) {
+    
+        callNotification()
+        print("ready")
+    }
+    
+    
+    
+    
+    
+    func callNotification() {
+    
+        
+        
+       // let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: triggerDate)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        let date = formatter.date(from: textDate.text!)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Appointment"
+        content.subtitle = (date?.description)!
+        content.body = "Remeber your appointment"
+        content.badge = 1
+        
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date!)
+        
+        
+        let triggerDate = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+       // let triggerDate = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: triggerDate)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    
+    
+    
+    
+    
+    }
+    
+    
+    // test keyboard
+    
+    @IBAction func tapGestureAction(_ sender: UITapGestureRecognizer) {
+    
+        self.view.endEditing(true)
 
+    }
+    
+    
+    
+    
 }
 
